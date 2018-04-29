9e6a6b2313e839f865c3c6e4b55bc33ced3f50cf
Cedric van Putten <me@bycedric.com>
2018-04-29 13:09:11 +0200

    feature(node-sentry-ci): add cdn update script and add all binary versions
    This will greatly increase the support for older versions and allows for quick updating when a new version is released.

---
+++ node-sentry-ci/pull-binaries.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+tags=`curl https://registry.hub.docker.com/v1/repositories/getsentry/sentry-cli/tags?page_size=1024 | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}'`
+versions=(${tags//$'\n'/ })
+
+echo "Pulled tags ${tags}"
+
+# see https://github.com/getsentry/sentry-cli/issues/227#issuecomment-358573511
+for tag in "${versions[@]}"
+do
+	docker pull getsentry/sentry-cli:$tag;
+	container=$(docker create getsentry/sentry-cli:$tag);
+	mkdir -p "./cdn/${tag}";
+	docker cp $container:/bin/sentry-cli cdn/$tag/sentry-cli-Linux-x86_64;
+	echo "${tag} done!";
+done
