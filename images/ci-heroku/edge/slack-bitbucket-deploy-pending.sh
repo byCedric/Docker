63adf8ddeedcafa4f76a9f808ab9ec659e0b1914
Cedric van Putten <me@bycedric.com>
2019-05-01 18:24:04 +0200

    fix(ci-heroku): add missing bitbucket workspace and specify the source

--- images/ci-heroku/edge/slack-bitbucket-deploy-pending.sh
+++ images/ci-heroku/edge/slack-bitbucket-deploy-pending.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 COMMIT_HASH="${BITBUCKET_COMMIT:0:7}"
 VERSION="${BITBUCKET_TAG:=$COMMIT_HASH}"
-URL="https://bitbucket.org/${BITBUCKET_REPO_SLUG}/addon/pipelines/home#!/results/${BITBUCKET_BUILD_NUMBER}"
+IDENTIFIER="${BITBUCKET_BRANCH:=$VERSION}"
+URL="https://bitbucket.org/${BITBUCKET_WORKSPACE}/${BITBUCKET_REPO_SLUG}/addon/pipelines/home#!/results/${BITBUCKET_BUILD_NUMBER}"
 
 CMD="slack chat send"
 CMD="$CMD --color unkown"
 CMD="$CMD --fallback \"Deploying version *$VERSION* - $BITBUCKET_REPO_SLUG - $URL\""
-CMD="$CMD --title \"$BITBUCKET_REPO_SLUG\""
+CMD="$CMD --title \"$BITBUCKET_REPO_SLUG - $IDENTIFIER\""
 CMD="$CMD --title-link \"$URL\""
 CMD="$CMD --text \"Deploying version *$VERSION*...\""
 CMD="$CMD --author \"Bitbucket Pipelines #$BITBUCKET_BUILD_NUMBER\""
